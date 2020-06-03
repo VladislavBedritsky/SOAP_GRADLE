62d3519b050a729b37583aed15daf24d2a2aafff
Vladislav Bedritsky <vladquinn2010@gmail.com>
2020-06-03 18:22:20 +0300

    configure app

---
+++ web/src/main/java/org/example/web/rest/CountryController.java
@@ -0,0 +1,23 @@
+package org.example.web.rest;
+
+
+import org.example.xsd.country.Country;
+import org.example.xsd.country.Currency;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryController {
+
+
+    @GetMapping
+    public Country getCountry() {
+        Country country = new Country();
+        country.setName("Russia");
+        country.setCapital("Moscow");
+        country.setCurrency(Currency.EUR);
+        country.setPopulation(1000);
+        return country;
+    }
+
+}
