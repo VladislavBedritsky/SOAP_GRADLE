058b9559988f9d4aed1ba37e6eae5ada074c23c7
Vladislav Bedritsky <vladquinn2010@gmail.com>
2020-06-05 18:16:41 +0300

    finished with soap consumer and producer

--- soap-consumer/src/main/java/org/example/consumer/client/CountryClient.java
+++ soap-consumer/src/main/java/org/example/consumer/client/CountryClient.java
@@ -1,11 +1,9 @@
 package org.example.consumer.client;
 
-import org.example.xsd.country.GetCountryByNameRequest;
-import org.example.xsd.country.GetCountryByNameResponse;
+import org.example.xsd.country.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
-import org.springframework.ws.soap.client.core.SoapActionCallback;
 
 public class CountryClient extends WebServiceGatewaySupport {
 
@@ -19,11 +17,46 @@ public class CountryClient extends WebServiceGatewaySupport {
         LOGGER.info("Requesting location for " + country);
 
         GetCountryByNameResponse response = (GetCountryByNameResponse) getWebServiceTemplate()
-                .marshalSendAndReceive("http://localhost:8080/ws/countries", request,
-                        new SoapActionCallback(
-                                "http://example.org/xsd/country"));
+                .marshalSendAndReceive("http://localhost:8080/ws/countries", request);
 
         return response;
+    }
+
+    public GetAllCountriesResponse getAllCountries() {
+
+        GetAllCountriesRequest request = new GetAllCountriesRequest();
+        LOGGER.info("Request is {}", request);
+
+        return (GetAllCountriesResponse) getWebServiceTemplate()
+                .marshalSendAndReceive("http://localhost:8080/ws/countries", request);
+
+    }
+
+    public void deleteCountry(Integer id) {
+        GetDeleteCountryRequest request = new GetDeleteCountryRequest();
+        request.setId(id);
+        LOGGER.info("Request is {}", request);
+
+        getWebServiceTemplate()
+                .marshalSendAndReceive("http://localhost:8080/ws/countries", request);
+    }
+
+    public void updateCountry(Country country) {
+        GetUpdateCountryRequest request = new GetUpdateCountryRequest();
+        request.setCountry(country);
+        LOGGER.info("Request is {}", request);
+
+        getWebServiceTemplate()
+                .marshalSendAndReceive("http://localhost:8080/ws/countries", request);
+    }
+
+    public void saveCountry(Country country) {
+        GetAddCountryRequest request = new GetAddCountryRequest();
+        request.setCountry(country);
+        LOGGER.info("Request is {}", request);
+
+        getWebServiceTemplate()
+                .marshalSendAndReceive("http://localhost:8080/ws/countries", request);
     }
 
 }
--- soap-consumer/src/main/java/org/example/consumer/rest/CountryController.java
+++ soap-consumer/src/main/java/org/example/consumer/rest/CountryController.java
@@ -1,12 +1,11 @@
 package org.example.consumer.rest;
 
 import org.example.consumer.client.CountryClient;
+import org.example.xsd.country.Country;
+import org.example.xsd.country.GetAllCountriesResponse;
 import org.example.xsd.country.GetCountryByNameResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/countries")
@@ -16,8 +15,31 @@ public class CountryController {
     private CountryClient countryClient;
 
     @GetMapping
-    public GetCountryByNameResponse getCountryByName(@RequestParam String name) {
+    public GetAllCountriesResponse findAll() {
+        return countryClient.getAllCountries();
+    }
+
+    @GetMapping("/{name}")
+    public GetCountryByNameResponse getCountryByName(@PathVariable String name) {
         return countryClient.getCountryByName(name);
     }
+
+    @PostMapping("/save")
+    public Country saveCountry(@RequestBody Country country) {
+        countryClient.saveCountry(country);
+        return country;
+    }
+
+    @PutMapping("/update")
+    public Country updateCountry(@RequestBody Country country) {
+        countryClient.updateCountry(country);
+        return country;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteCountry(@PathVariable Integer id) {
+        countryClient.deleteCountry(id);
+    }
+
 
 }
